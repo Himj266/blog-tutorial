a16c64da8fec6e7532c285b23d68eb808d2afc00
Himanshu Jain <jainh246@gmail.com>
2023-01-25 23:47:39 +0530

    inintal commit

---
+++ app/routes/index.tsx
@@ -0,0 +1,10 @@
+import { Link } from "@remix-run/react";
+
+import { useOptionalUser } from "~/utils";
+
+export default function Index() {
+  const user = useOptionalUser();
+  return (
+   <main>Hi</main>
+  );
+}
